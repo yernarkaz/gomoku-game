dbc7901d42e53e7255567703937ce63b0acefd3c
Yernar <yernarkaz@gmail.com>
2024-09-30 23:01:20 +0200

    Configure the game at the entry point.

--- run.py
+++ run.py
@@ -2,7 +2,20 @@
 
 from classes.game import Game
 
+import yaml
+
+GAME_CONFIG_PATH = "config/game.yml"
+
 print(__name__)
 if __name__ == "__main__":
-    game = Game()
+    with open(GAME_CONFIG_PATH, "r") as file:
+        game_config = yaml.safe_load(file)
+        print("game config loaded safely")
+        print(game_config)
+
+    for config_name, path in game_config["config_paths"].items():
+        with open(path, "r") as file:
+            game_config[config_name] = yaml.safe_load(file)
+
+    game = Game(game_config)
     game.run()
